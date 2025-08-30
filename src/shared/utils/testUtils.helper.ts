b95a84564f2b3a93030194951097ec3d07fcb5fe
Nitram <nitramcay@gmail.com>
2025-08-30 10:16:12 +0200

    fix: Phenix

--- src/shared/utils/testUtils.helper.ts
+++ src/shared/utils/testUtils.helper.ts
@@ -228,7 +228,7 @@ export const createPlayerWithPowerUp = (
     [PowerUpType.SLOW_MOTION]: 1,
     [PowerUpType.MULTI_SHOT]: 1,
     [PowerUpType.RICE_ROCKET_AMMO]: 1,
-    [PowerUpType.PHOENIX_PACT]: 1,
+    [PowerUpType.PHOENIX_PACT]: 0,
   };
 
   // Set the specific power-up as active
